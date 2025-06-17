73037d877147aaf6e70e7f6cb18fcffee5a7810f
Spencer Wilson <165120193+Swilsondi@users.noreply.github.com>
2025-06-16 20:22:58 -0400

    Adds MongoDB connection and JSON parsing middleware
    Establishes a connection to MongoDB using the URI from environment variables, enabling the app to interact with the database.
    
    Incorporates middleware for parsing JSON request bodies, improving API functionality for handling incoming data.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
