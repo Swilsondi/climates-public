73b246d559293780c2ef9abda44720a433ec918a
Spencer Wilson <165120193+Swilsondi@users.noreply.github.com>
2025-06-18 09:20:17 -0400

    Adds user creation, deletion, and update functionalities
    Implements new user management features including the ability to create, delete, and update users by ID.
    
    Refactors existing user retrieval methods for consistency by changing success response to a boolean value.
    
    Enhances error handling and ensures appropriate success messages are returned for each operation.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,7 @@ exports.getAllUsers = async (req, res, next) => {
     const users = await User.find();
     // Fetch all users from the database
     res.status(200).json({
-      success: "success",
+      success: true,
       data: users,
       message: "Users retrieved successfully",
     });
@@ -17,7 +17,7 @@ exports.getAllUsers = async (req, res, next) => {
 };
 
 // Get user by ID
-exoorts.getUserId = async (req, res, next) => {
+exports.getUserById = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
     if (!user) {
@@ -28,9 +28,68 @@ exoorts.getUserId = async (req, res, next) => {
       });
     }
     res.status(200).json({
-      success: "success",
+      success: true,
       data: user,
       message: "User retrieved successfully by ID",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Create a new user
+exports.createUser = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body); // Saves the new user to the database
+    res.status(201).json({
+      success: true,
+      data: newUser,
+      message: "User created successfully",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Delete user by ID
+exports.deleteUserById = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+        error: "No user found with the provided ID",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+      data: user,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Update user by ID
+exports.updateUserById = async (req, res, next) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User can not be found",
+        data: updatedUser,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: updatedUser,
+      message: "User updated successfully",
     });
   } catch (err) {
     next(err);
